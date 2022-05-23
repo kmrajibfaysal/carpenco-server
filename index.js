7d1abb47aafd51b07ce49dea638214249c54e017
kmrajibfaysal <kmrajibfaysal@gmail.com>
2022-05-23 06:45:04 +0600

    added method to parse data from database and send to client get req

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -12,6 +13,29 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send(`Doctors portal server running on port ${port}`);
 });
+
+// Mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fakac.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+const productCollection = client.db('Carpenco').collection('products');
+
+async function run() {
+    await client.connect();
+    console.log('database connected');
+
+    app.get('/products', async (req, res) => {
+        const products = await productCollection.find().toArray();
+        res.send(products);
+    });
+}
+run().catch(console.dir);
+
 // Listener
 app.listen(port, () => {
     console.log(`Doctors portal server running on port ${port}`);
